592ed527a3d40203cc1421c5a0e875aa57e2dbc2
dpcpointer <heapallocation@proton.me>
2025-06-03 16:00:03 -0600

    Add files via upload

---
+++ source.h
@@ -0,0 +1,12 @@
+#pragma once	
+
+#include <ntifs.h>
+#include <ntddk.h>
+#include <intrin.h>
+#include "utils.h"
+#include "hexrays.h"
+
+PKPRCB KeGetCurrentPrcb();
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath);
+void DriverUnload(PDRIVER_OBJECT DriverObject);
+BOOLEAN IsPatchGuardDpc(__int64 DeferredContext);
